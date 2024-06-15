6fc911b689eaa60717c409409525d88822568b2e
Artem Sapegin <artem@sapegin.ru>
2024-06-15 07:16:47 +0200

    Lint iTerm theme

--- scripts/lint.js
+++ scripts/lint.js
@@ -4,15 +4,28 @@
 
 import fs from 'fs';
 import path from 'path';
+import _ from 'lodash';
 import { glob } from 'glob';
 import stripJsonComments from 'strip-json-comments';
 import terminalLink from 'terminal-link';
 import rgbHex from 'rgb-hex';
 import cssColorNames from 'css-color-names' with { type: 'json' };
 
-// TODO: iTerm, Terminal.app
+// TODO: Terminal.app
 
 let errorCount = 0;
+
+const EXTENSIONS = [
+  'alfredappearance',
+  'css',
+  'itermcolors',
+  'json',
+  'lua',
+  'palette',
+  'theme',
+  'tmTheme',
+  'toml',
+].join(',');
 
 const IGNORES = [
   'package.json',
@@ -20,108 +33,137 @@ const IGNORES = [
   'SquirrelsongLightDarkTerminal.color-theme.json',
 ];
 
-const EXCEPTIONS_LIGHT = [
+const TRANSPARENT = [
   // Transparent colors
   '#ffffff00',
   '#ffffffff',
-  // Slack system navigation: existing colors look too intense (this color isn't
-  // used as is by Slack but is "adjusted" and other colors are made based on
-  // this color)
-  '#c3ccd6',
-];
-// Extended palette (only for JetBrains)
-const EXTENDED_LIGHT = [
-  '#1d1d1f',
-  '#272629',
-  '#373538',
-  '#49474a',
-  '#5b595e',
-  '#78737d',
-  '#87868a',
-  '#a2a1a6',
-  '#9c96a2',
-  '#d2cfd4',
-  '#dbd7e0',
-  '#e8e5eb',
-  '#f7f6f9',
-  '#fdfdfe',
-  '#61778c',
-  '#678499',
-  '#6f90a6',
-  '#789ab3',
-  '#80a4be',
-  '#8db2cc',
-  '#9ec0d9',
-  '#b7d3e8',
-  '#c9ddec',
-  '#d7e8f5',
-  '#e2edf5',
-  '#e9f1f7',
-  '#f3f9fc',
-  '#77805d',
-  '#838c66',
-  '#8f9970',
-  '#9ba679',
-  '#9bae7e',
-  '#b5bf8a',
-  '#becc99',
-  '#ced9a3',
-  '#d6e6ac',
-  '#e4f2d5',
-  '#f2fae1',
-  '#b6932c',
-  '#c49f37',
-  '#cba63b',
-  '#d9b754',
-  '#e6c565',
-  '#fcdfa5',
-  '#ffebbf',
-  '#faebcc',
-  '#fff8e9',
-  '#fdfbf5',
-  '#99453d',
-  '#a64b42',
-  '#b35047',
-  '#bf564c',
-  '#d67e76',
-  '#d9756c',
-  '#e6938a',
-  '#f2b4aa',
-  '#ebbfbc',
-  '#f7d5d2',
-  '#f5e5e4',
-  '#fcf6f5',
-  '#a67642',
-  '#b37f47',
-  '#bf884c',
-  '#cc9152',
-  '#de9e59',
-  '#d9ab79',
-  '#edcda8',
-  '#f2dec9',
-  '#fcf1e6',
-  '#3c665c',
-  '#457367',
-  '#4f8076',
-  '#538c7f',
-  '#5f9b8d',
-  '#6ca899',
-  '#81b6a9',
-  '#a9d5cb',
-  '#ceece5',
-  '#877a99',
-  '#9085a6',
-  '#9d8fb3',
-  '#a899bf',
-  '#ac9bc5',
-  '#bfadd9',
-  '#d1c3e7',
-  '#dfd2f3',
-  '#e7def5',
-  '#ede7f6',
 ];
 
-const EXCEPTIONS_DARK = EXCEPTIONS_LIGHT;
+const EXCEPTIONS = {
+  'light/JetBrains/squirrelsong-light/resources/theme/Squirrelsong Light.theme.json':
+    [
+      '#1d1d1f',
+      '#272629',
+      '#373538',
+      '#49474a',
+      '#5b595e',
+      '#78737d',
+      '#87868a',
+      '#a2a1a6',
+      '#9c96a2',
+      '#d2cfd4',
+      '#dbd7e0',
+      '#e8e5eb',
+      '#f7f6f9',
+      '#fdfdfe',
+      '#61778c',
+      '#678499',
+      '#6f90a6',
+      '#789ab3',
+      '#80a4be',
+      '#8db2cc',
+      '#9ec0d9',
+      '#b7d3e8',
+      '#c9ddec',
+      '#d7e8f5',
+      '#e2edf5',
+      '#e9f1f7',
+      '#f3f9fc',
+      '#77805d',
+      '#838c66',
+      '#8f9970',
+      '#9ba679',
+      '#9bae7e',
+      '#b5bf8a',
+      '#becc99',
+      '#ced9a3',
+      '#d6e6ac',
+      '#e4f2d5',
+      '#f2fae1',
+      '#b6932c',
+      '#c49f37',
+      '#cba63b',
+      '#d9b754',
+      '#e6c565',
+      '#fcdfa5',
+      '#ffebbf',
+      '#faebcc',
+      '#fff8e9',
+      '#fdfbf5',
+      '#99453d',
+      '#a64b42',
+      '#b35047',
+      '#bf564c',
+      '#d67e76',
+      '#d9756c',
+      '#e6938a',
+      '#f2b4aa',
+      '#ebbfbc',
+      '#f7d5d2',
+      '#f5e5e4',
+      '#fcf6f5',
+      '#a67642',
+      '#b37f47',
+      '#bf884c',
+      '#cc9152',
+      '#de9e59',
+      '#d9ab79',
+      '#edcda8',
+      '#f2dec9',
+      '#fcf1e6',
+      '#3c665c',
+      '#457367',
+      '#4f8076',
+      '#538c7f',
+      '#5f9b8d',
+      '#6ca899',
+      '#81b6a9',
+      '#a9d5cb',
+      '#ceece5',
+      '#877a99',
+      '#9085a6',
+      '#9d8fb3',
+      '#a899bf',
+      '#ac9bc5',
+      '#bfadd9',
+      '#d1c3e7',
+      '#dfd2f3',
+      '#e7def5',
+      '#ede7f6',
+    ],
+  'dark/WezTerm/squirrelsong-dark.toml': [
+    '#080706',
+    '#12100e',
+    '#1c1916',
+    '#26221f',
+    '#302c27',
+    '#3b352f',
+    '#453e37',
+    '#4f473f',
+    '#595047',
+    '#61574e',
+    '#6b6056',
+    '#756a5e',
+    '#807366',
+    '#8a7c6e',
+    '#948576',
+    '#9e8e7e',
+    '#a89787',
+    '#b3a18f',
+    '#bdaa97',
+    '#c7b39f',
+    '#d1bca7',
+    '#d9c3ad',
+    '#e3ccb6',
+    '#edd5be',
+  ],
+  'light/Slack/colors.json': [
+    // Slack system navigation: existing colors look too intense (this color isn't
+    // used as is by Slack but is "adjusted" and other colors are made based on
+    // this color)
+    '#c3ccd6',
+  ],
+};
 
 const CUSTOM_LINTERS = [
   {
@@ -155,6 +197,24 @@ const CUSTOM_LINTERS = [
       });
     },
   },
+  {
+    // iTerm
+    condition: (file) => file.endsWith('.itermcolors'),
+    lintFunction: (file, validColors, exceptions) => {
+      const text = fs.readFileSync(file, 'utf8');
+
+      const matches = text.match(/<real>[^<]*<\/real>/gi);
+      const numbers = matches.map((x) => Number(x.replace(/<\/?real>/gi, '')));
+      const colors = _.chunk(numbers, 4);
+
+      colors.forEach(([a, b, g, r]) => {
+        const color = `#${rgbHex(r * 255, g * 255, b * 255, a)}`;
+        if (isValidHexColor(color, validColors, exceptions) === false) {
+          achtung(`${color} (${r}, ${g}, ${b}, ${a})`);
+        }
+      });
+    },
+  },
 ];
 
 function achtung(value, description) {
@@ -180,13 +240,13 @@ function isHexColor(value) {
   return /^#[0-9a-f]{3,8}$/i.test(value);
 }
 
-function isValidHexColor(value, validColors, exceptions, extended = []) {
+function isValidHexColor(value, validColors, exceptions) {
   const color = value.toLowerCase();
 
-  if (exceptions.includes(color)) {
+  if (TRANSPARENT.includes(color)) {
     return true;
   }
-  if (extended.includes(color)) {
+  if (exceptions.includes(color)) {
     return true;
   }
   if (validColors.includes(color)) {
@@ -195,12 +255,7 @@ function isValidHexColor(value, validColors, exceptions, extended = []) {
 
   // Validate colors with alpha channel as regular HEX: #c0ffeeff -> #c0ffee
   if (color.length === 9) {
-    return isValidHexColor(
-      color.slice(0, 7),
-      validColors,
-      exceptions,
-      extended,
-    );
+    return isValidHexColor(color.slice(0, 7), validColors, exceptions);
   }
 
   return false;
@@ -216,40 +271,35 @@ function scanObject(obj, callback) {
   }
 }
 
-function lintJson(file, validColors, exceptions, extended) {
+function lintJson(file, validColors, exceptions) {
   let theme;
   try {
     theme = readJsonFile(file);
   } catch (err) {
-    lintText(file, validColors, exceptions, extended);
+    lintText(file, validColors, exceptions);
     return;
   }
-
-  const extendedPalette = file.includes('JetBrains') ? extended : [];
 
   // console.log('🦜', theme);
   scanObject(theme, (value) => {
     if (isCssNamedColor(value)) {
-      // TODO
+      // TODO: Skip named colors for now, they are only used in JetBrains
       // achtung(value);
       return;
     }
     if (isHexColor(value)) {
       const color = value.toLowerCase();
-      if (
-        isValidHexColor(color, validColors, exceptions, extendedPalette) ===
-        false
-      ) {
+      if (isValidHexColor(color, validColors, exceptions) === false) {
         achtung(value);
       }
     }
   });
 }
 
-function lintText(file, validColors, exceptions, extended) {
+function lintText(file, validColors, exceptions) {
   const text = fs.readFileSync(file, 'utf8');
 
-  const matches = text.match(/#[0-9a-f]{3,8}\b/i);
+  const matches = text.match(/#[0-9a-f]{3,8}\b/gi);
 
   matches.forEach((color) => {
     if (isValidHexColor(color, validColors, exceptions) === false) {
@@ -258,11 +308,9 @@ function lintText(file, validColors, exceptions, extended) {
   });
 }
 
-function lint(root, palette, exceptions, extended) {
+function lint(root, palette, exceptions) {
   const validColors = Object.values(palette);
-  const themes = glob.sync(
-    `${root}/*/**/*.{json,css,lua,alfredappearance,theme,tmTheme,palette}`,
-  );
+  const themes = glob.sync(`${root}/*/**/*.{${EXTENSIONS}}`);
   themes.forEach((file) => {
     const filename = path.basename(file);
     if (IGNORES.includes(filename) || file.includes('node_modules')) {
@@ -275,9 +323,11 @@ function lint(root, palette, exceptions, extended) {
       terminalLink(file, `vscode://file//${process.cwd()}/${file}`),
     );
 
+    const exceptions = EXCEPTIONS[file] ?? [];
+
     for (const { condition, lintFunction } of CUSTOM_LINTERS) {
       if (condition(file)) {
-        lintFunction(file, validColors, exceptions, extended);
+        lintFunction(file, validColors, exceptions);
         return;
       }
     }
@@ -286,16 +336,10 @@ function lint(root, palette, exceptions, extended) {
       case '.json':
       case '.theme':
       case '.alfredappearance':
-        lintJson(file, validColors, exceptions, extended);
-        break;
-      case '.css':
-      case '.lua':
-      case '.tmTheme':
-      case '.palette':
-        lintText(file, validColors, exceptions, extended);
+        lintJson(file, validColors, exceptions);
         break;
       default:
-        console.error('Unknown file type', file);
+        lintText(file, validColors, exceptions);
     }
   });
 }
@@ -303,12 +347,12 @@ function lint(root, palette, exceptions, extended) {
 console.log();
 console.log('[LINT] Linting light themes... 🌞');
 const lightPalette = readJsonFile('light/palette.json');
-lint('light', lightPalette, EXCEPTIONS_LIGHT, EXTENDED_LIGHT);
+lint('light', lightPalette);
 
 console.log();
 console.log('[LINT] Linting dark themes... 🌚');
 const darkPalette = readJsonFile('dark/palette.json');
-lint('dark', darkPalette, EXCEPTIONS_DARK, []);
+lint('dark', darkPalette);
 
 console.log();
 console.log(`[LINT] ${errorCount} errors found 🦜`);
