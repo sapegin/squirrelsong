cfd7b636579dd2e2423179f8e503f2ee6c083fc8
Artem Sapegin <artem@sapegin.ru>
2025-07-28 15:18:24 +0200

    Lint: Support the new repo structure with all themes in the themes/ folder (#27)

--- scripts/lib/cssColorNames.mjs
+++
@@ -1,7 +0,0 @@
-// HACK: ESLint doesn't yet support `width` suffixes,
-// this file is ignored in ESLint config
-// https://github.com/eslint/eslint/discussions/15305
-
-import cssColorNames from 'css-color-names' with { type: 'json' };
-
-export { cssColorNames };
