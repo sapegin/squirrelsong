83568521c9e9635fab2f6c0a7b94d1aaf454fec7
Artem Sapegin <artem@sapegin.me>
2026-06-27 14:12:23 +0200

    Fzf: Fix colors
    Regular isn't a thing...

--- themes/Zsh/fzf/fzf-squirrelsong.sh
+++ themes/Zsh/fzf/fzf-squirrelsong.sh
@@ -1,17 +1,17 @@
 # Squirrelsong for fzf
 export FZF_COLORS="fg:-1,\
-fg+:regular,\
+fg+:-1,\
 bg:-1,\
-bg+:regular,\
+bg+:235,\
 hl:bright-red,\
 hl+:bright-red,\
 info:bright-black,\
-marker:yellow,\
+marker:bright-red,\
 prompt:bright-black,\
-spinner:regular,\
-pointer:regular,\
+spinner:black,\
+pointer:bright-red,\
 header:bright-white,\
 border:black,\
-label:regular,\
+label:yellow,\
 query:bright-white,\
 disabled:bright-black"
